19646a9ce6569644723644e61df6a65e430cf82d
Marcel <marcel.bernet@ch-open.ch>
2020-06-17 15:47:20 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Modul W906!" );
+        System.out.println( "Hello Modul W906 am Tag 6!" );
     }
 }
